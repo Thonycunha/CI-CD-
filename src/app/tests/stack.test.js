ea9a9fe518b847e93c06ee23191c503438ce884b
thonysilva <thony.libresolucoes@gmail.com>
2023-11-30 21:50:10 -0300

    Adicionando testes básicos

---
+++ src/app/tests/stack.test.js
@@ -0,0 +1,33 @@
+class Stack {
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+  get peek(){
+    return this.items[this.top]
+  }
+
+  push(values) {
+    this.top += 1;
+    this.items[this.top] = values;
+  }
+}
+
+describe("My stack", () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it("ta vazio", () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+
+  it("pode jogar pro alto", () => {
+    stack.push('1');
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('1');
+  });
+});
